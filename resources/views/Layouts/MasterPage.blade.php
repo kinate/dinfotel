6f7699b18ddc43c9ecb3b4fb05a7088fb7e455c9
kinate <kinate82@gmail.com>
2023-01-12 11:09:37 +0300

    Removed Pre-Load

--- resources/views/Layouts/MasterPage.blade.php
+++ resources/views/Layouts/MasterPage.blade.php
@@ -48,13 +48,13 @@
 		<div class="offwrap"></div>
 
 		<!--Preloader start here-->
-	   	<div id="pre-load">
+	   	{{-- <div id="pre-load">
             <div id="loader" class="loader">
                 <div class="loader-container">
                     <div class="loader-icon"><img src="assets/images/favicon.png" alt="Konstruk - Construction & Building Html Template "></div>
                 </div>
             </div>              
-        </div>
+        </div> --}}
 		<!--Preloader area end here-->
 			
 		<!-- Main content Start -->
